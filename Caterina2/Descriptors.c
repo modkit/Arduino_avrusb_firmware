473da228eeb8261a586e9e9bd2e8367cb3542700
Benjamin Riggs <deisum@gmail.com>
2019-05-25 14:50:18 -0400

    update CALLBACK_USB_GetDescriptor(...) signature to newer LUFA API

--- Caterina2/Descriptors.c
+++ Caterina2/Descriptors.c
@@ -229,7 +229,7 @@ const USB_Descriptor_String_t DESCRIPTOR_PROGMEM ProductString = USB_STRING_DESC
  *  USB host.
  */
 uint16_t CALLBACK_USB_GetDescriptor(const uint16_t wValue,
-                                    const uint8_t wIndex,
+                                    const uint16_t wIndex,
                                     const void** const DescriptorAddress)
 {
 	const uint8_t  DescriptorType   = (wValue >> 8);
